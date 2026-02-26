cc017e6f641a86c7ec1483d9aff6920d3f9405d0
Siryll <zumofencyril@gmail.com>
2026-02-26 12:20:56 +0100

    implement no_std test widget

--- widget/src/lib.rs
+++ widget/src/lib.rs
@@ -1,88 +1,57 @@
-use serde_json::Error;
-use serde_json::Value;
+#![no_std]
 
-use schemars::{schema_for, JsonSchema};
-use serde::Deserialize;
+extern crate alloc;
 
-use widget::widget::{http, logging};
+use alloc::format;
+use core::panic::PanicInfo;
+use wit_bindgen::rt::string::String;
+
+use widget::widget::logging;
 
 wit_bindgen::generate!({
     path: "../wg_display_widget_wit/wit",
     world: "widget"
 });
 
-#[derive(JsonSchema, Deserialize)]
-struct WidgetConfig {
-    city: String,
-}
+const WIDGET_NAME: &str = "ESP32 Runtime Test Widget";
 
-const WIDGET_NAME: &str = "Rust Widget Template";
+#[global_allocator]
+static ALLOCATOR: dlmalloc::GlobalDlmalloc = dlmalloc::GlobalDlmalloc;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo<'_>) -> ! {
+    loop {}
+}
 
 struct MyWidget;
 
 impl Guest for MyWidget {
-    fn get_name() -> wit_bindgen::rt::string::String {
+    fn get_name() -> String {
         WIDGET_NAME.into()
     }
 
     fn run(context: WidgetContext) -> WidgetResult {
-        // Widgets can log to the console
-        logging::log(logging::Level::Info, WIDGET_NAME, "Widget run started");
+        logging::log(logging::Level::Info, WIDGET_NAME, "run() invoked");
 
-        // Widgets can handle the case where no config is provided
-        if "{}" == context.config {
-            return WidgetResult {
-                data: "No config provided".into(),
-            };
+        WidgetResult {
+            data: format!(
+                "TEST_OK: run called, last_invocation={}s, config='{}'",
+                context.last_invocation.seconds,
+                context.config
+            ),
         }
-
-        // Widgets can parse their config with ease using serde
-        let config: WidgetConfig =
-            serde_json::from_str(&context.config).expect("Failed to parse config");
-
-        // Widgets can make network requests
-        let response = http::request(
-            http::Method::Get,
-            format!(
-                "https://aareguru.existenz.ch/v2018/today?city={}",
-                config.city
-            )
-            .as_str(),
-            None,
-        );
-        let Ok(response) = response else {
-            return WidgetResult {
-                data: "Failed to make network request".into(),
-            };
-        };
-
-        if 200 != response.status {
-            return WidgetResult {
-                data: format!("Response status != 200: {}", response.status),
-            };
-        }
-
-        let data: Result<Value, Error> = serde_json::from_slice(response.bytes.as_slice());
-        let result = match data {
-            Ok(data) => format!("Aare Temperature in {}: {} C", config.city, data["aare"]),
-            Err(_) => "Response from AareGuru could not be parsed".into(),
-        };
-
-        WidgetResult { data: result }
     }
 
-    fn get_config_schema() -> wit_bindgen::rt::string::String {
-        let schema = schema_for!(WidgetConfig);
-        serde_json::to_string_pretty(&schema).unwrap()
+    fn get_config_schema() -> String {
+        "{\"type\":\"object\",\"properties\":{},\"additionalProperties\":false}".into()
     }
 
-    fn get_version() -> wit_bindgen::rt::string::String {
-        "1.0.0".into()
+    fn get_version() -> String {
+        "0.0.1-test".into()
     }
 
     fn get_run_update_cycle_seconds() -> u32 {
-        // This widget shall be updated every second
-        1
+        5
     }
 }
 
